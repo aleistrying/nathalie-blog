dc633fff312be7a05f26901d13a3fed74154232f
Alejandro P <alejandroparparcen@yahoo.com>
2024-01-11 13:06:46 -0500

    Refactor PdfCard component styling. now it hsould only show the pdf view if clicked and the title and everythign has betterer styles

--- components/pdfCard.tsx
+++ components/pdfCard.tsx
@@ -11,23 +11,32 @@ export default function PdfCard({ pdf }) {
     <div
       className={`card
             flex transform cursor-pointer
-            flex-col overflow-hidden rounded-lg bg-gray-300 bg-opacity-30 
-            p-5
-            shadow-lg backdrop-blur-lg backdrop-filter transition duration-500 ease-in-out dark:bg-gray-900 dark:bg-opacity-40
+flex-row items-center justify-between overflow-hidden rounded-lg bg-gray-300 
+            bg-opacity-30            shadow-lg backdrop-blur-lg backdrop-filter transition duration-200 ease-in-out dark:bg-gray-900 dark:bg-opacity-40
             dark:text-black
 ${
-  isExpanded
-    ? "absolute left-0 top-0 z-30 h-screen w-full"
-    : "h-64  hover:-translate-y-1 hover:scale-110"
+  // isExpanded
+  //   ? "absolute left-0 top-0 z-30 h-screen w-full"
+  //       :
+  "  hover:-translate-y-1 hover:scale-110"
 }	
           `}
       onClick={handleClick}
       key={pdf._id}>
-      <h2
-        className={`mb-2 flex flex-row items-center justify-center text-2xl font-bold text-gray-800 dark:text-gray-50 `}>
-        {pdf.title}
-      </h2>
-      {pdf?.file?.asset?._ref?.includes("pdf") ? (
+      <div className="w-2/3 min-w-0 p-2">
+        <h2 className="flex w-full max-w-full select-none flex-row items-center justify-center whitespace-normal break-words break-all text-xl text-gray-800 dark:text-gray-50">
+          {pdf?.title ?? "Sin título"}
+        </h2>
+      </div>
+      {/* add a pretty button to open pdf in red */}
+      <button
+        className="h-full w-1/3 rounded-r bg-red-500 px-4 py-2 font-bold  text-white backdrop-blur-lg backdrop-filter hover:bg-red-700"
+        onClick={() => {
+          window.open(pdf.fileUrl, "_blank");
+        }}>
+        VER PDF
+      </button>
+      {/* {pdf?.file?.asset?._ref?.includes("pdf") ? (
         <iframe
           className={`relative cursor-pointer rounded-sm ${
             !isExpanded
@@ -55,9 +64,9 @@ ${
           width={1080}
           height={1080}
         />
-      )}
+      )} */}
       {/* add closing controls */}
-      {isExpanded && (
+      {/* {isExpanded && (
         <div className="absolute right-0 top-0 m-2">
           <button
             className="text-xl font-bold text-gray-800"
@@ -65,7 +74,7 @@ ${
             X (Cerrar)
           </button>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
